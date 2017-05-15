2640b512f9c4650330e3b4da3842c733652f3020
Gilles Aroutzet <aroutzet@gmail.com>
2017-05-15 18:16:05 +0200

    correction routes + controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :spacelifts, only [ :index, :show, :new :create ] do
+  resources :corridors, only [ :index, :show, :new :create ], shallow: true do
     resources :bookings, only [ :new, :create ]
   end
 
