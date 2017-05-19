cf98cc9cc049792cd01ee2436f86889f52e8654f
Abdallah Chaib <abduchaib@hotmail.fr>
2017-05-19 15:40:29 +0200

    c cool

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    bookings_path
+  end
 end
